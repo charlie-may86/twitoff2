fd3f38dc198ff1d0cc0bb710ff75aefbc8ee0ac7
Charlie May <camiv95@gmail.com>
2020-08-10 23:01:13 -0500

    working app

--- twitoff/predict.py
+++ twitoff/predict.py
@@ -1,31 +1,30 @@
-'''Prediction of users based on tweet embeddings'''
+'''Prediction of Users based on Tweet embeddings'''
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from .db_model import User
 from .twitter import BASILICA
-import numpy as np
+
 
 def predict_user(user1, user2, tweet_text):
     '''Determine and return which user is more likely to say a given tweet.
-
     # Arguments:
-        user1: str, twitter user name for user1 in comparisson
-        user2: str, twitter user name for user1 in comparisson
+        user1: str, twitter user name for user 1 in comparison from web form
+        user2: str, twitter user name for user 1 in comparison from web form
         tweet_text: str, tweet text to evaluate
-
-    # Returns: 
+    # Returns:
         prediction from logistic regression model
     '''
-
     user1 = User.query.filter(User.username == user1).one()
-    user2 = User.query.filter(User.username == user2).two()
+    user2 = User.query.filter(User.username == user2).one()
     user1_embeddings = np.array([tweet.embedding for tweet in user1.tweet])
     user2_embeddings = np.array([tweet.embedding for tweet in user2.tweet])
-
-    # Create embeddings and create labels
+    
+    # Combine embeddings and create labels
     embeddings = np.vstack([user1_embeddings, user2_embeddings])
-    labels = np.concatenate([np.ones(len(user1_embeddings)), np.zeros(len(user2_embeddings))])
+    labels = np.concatenate([np.ones(len(user1_embeddings)), 
+                             np.zeros(len(user2_embeddings))])
 
-    # train model and convert imput text to embedding
+    # Train model and convert input text to embedding
     lr = LogisticRegression().fit(embeddings, labels)
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
 
